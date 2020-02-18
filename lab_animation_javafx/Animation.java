b3dd6ed53c481792d1625b836048717e725b8458
kevinkoudelka <kevinkoudelka@wifi-roaming-128-4-78-144.host.udel.edu>
2020-02-17 19:38:11 -0500

    Borders and Correct Images

--- lab_animation_javafx/Animation.java
+++ lab_animation_javafx/Animation.java
@@ -26,7 +26,7 @@ public class Animation extends Application {
     final static int imgWidth = 165;
     final static int imgHeight = 165;
 	 boolean hflag = true;
-	 boolean vflag = false;
+	 boolean vflag = true;
 
     // TODO: Change the code so that at least eight orc animation pngs are loaded
     public static void main(String[] args) {
@@ -44,8 +44,10 @@ public class Animation extends Application {
         Canvas canvas = new Canvas(canvasWidth, canvasHeight);
         root.getChildren().add(canvas);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        Image orc_img = createImage();
-
+        Image orc_img = createImage("/orc_animation/orc_forward_southeast.png");
+        Image orc_northwest = createImage("/orc_animation/orc_forward_northwest.png");
+        Image orc_northeast = createImage("/orc_animation/orc_forward_northeast.png");
+        Image orc_southwest = createImage("/orc_animation/orc_forward_southwest.png");
         final long startNanoTime = System.nanoTime();
 
         new AnimationTimer() {
@@ -71,8 +73,22 @@ public class Animation extends Application {
                 gc.clearRect(0, 0, canvasWidth, canvasHeight);
 
                 // draw the subimage from the original png to animate the orc's motion
+                if (hflag ==true && vflag == true) {
                 gc.drawImage(orc_img, imgWidth*picInd, 0, imgWidth, imgHeight,
                                     xloc, yloc, imgWidth, imgHeight);
+                }
+                else if (hflag == false && vflag == true) {
+                gc.drawImage(orc_southwest, imgWidth*picInd, 0, imgWidth, imgHeight,
+                        xloc, yloc, imgWidth, imgHeight);
+                }
+                else if (hflag == false && vflag == false) {
+                gc.drawImage(orc_northwest, imgWidth*picInd, 0, imgWidth, imgHeight,
+                       xloc, yloc, imgWidth, imgHeight);
+                }
+                else if (hflag == true && vflag == false) {
+                    gc.drawImage(orc_northeast, imgWidth*picInd, 0, imgWidth, imgHeight,
+                           xloc, yloc, imgWidth, imgHeight);
+                }
                 picInd = (picInd + 1) % canvasCount;
                 
                 try {
@@ -89,8 +105,8 @@ public class Animation extends Application {
     }
 
     //Read image from file and return
-    private Image createImage() {
-        Image img = new Image("/orc_animation/orc_forward_southeast.png");
+    private Image createImage(String x) {
+        Image img = new Image(x);
         return img;   	
     	// TODO: Change this method so you can load other orc animation bitmaps
     }
@@ -99,10 +115,10 @@ public class Animation extends Application {
 	 if (xloc >= 400) {
 		 hflag = false;
 	 }
-	 else if (xloc <= -100) {
+	 else if (xloc <= -50) {
 		 hflag = true;
 	 }
-	 else if (yloc <= 0) {
+	 else if (yloc <= -50) {
 		 vflag = true;
 	 }
 	 else if (yloc >= 200) {
